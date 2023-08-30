4939281b855a7e38877a8d7a952c80d18fe267e3
mahesh <deshmukh.mahesh.n@gmail.com>
2023-08-30 23:18:37 +0530

    css moved to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import "./styles/style.css";
-import "./styles/flaticon.css";
-import "./styles/icomoon.css";
-import "./styles/aos.css";
-import "./styles/ionicons.min.css";
-import "./styles/magnific-popup.css";
-import "./styles/owl.theme.default.min.css";
-import "./styles/animate.css";
-import "./styles/open-iconic-bootstrap.min.css";
-import "./styles/Font/FontAwesome/all.min.css";
+// import "./styles/style.css";
+// import "./styles/flaticon.css";
+// import "./styles/icomoon.css";
+// import "./styles/aos.css";
+// import "./styles/ionicons.min.css";
+// import "./styles/magnific-popup.css";
+// import "./styles/owl.theme.default.min.css";
+// import "./styles/animate.css";
+// import "./styles/open-iconic-bootstrap.min.css";
+// import "./styles/Font/FontAwesome/all.min.css";
 import type { Metadata } from "next";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -30,14 +30,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700"
           rel="stylesheet"
         />
-        {/* <link rel="stylesheet" href="styles/style.css"></link>
-        <link rel="stylesheet" href="styles/flaticon.css"></link>
-        <link rel="stylesheet" href="styles/icomoon.css"></link>
-        <link rel="stylesheet" href="styles/aos.css"></link>
-        <link rel="stylesheet" href="styles/ionicons.min.css"></link>
-        <link rel="stylesheet" href="styles/magnific-popup.css"></link>
-        <link rel="stylesheet" href="styles/owl.theme.default.min.css"></link>
-        <link rel="stylesheet" href="styles/animate.css"></link>
+        <link rel="stylesheet" href="./styles/style.css"></link>
+        <link rel="stylesheet" href="./styles/flaticon.css"></link>
+        <link rel="stylesheet" href="./styles/icomoon.css"></link>
+        <link rel="stylesheet" href="./styles/aos.css"></link>
+        <link rel="stylesheet" href="./styles/ionicons.min.css"></link>
+        <link rel="stylesheet" href="./styles/magnific-popup.css"></link>
+        <link rel="stylesheet" href="./styles/owl.theme.default.min.css"></link>
+        <link rel="stylesheet" href="./styles/animate.css"></link>
         <link
           rel="stylesheet"
           href="styles/open-iconic-bootstrap.min.css"
@@ -45,7 +45,7 @@ export default function RootLayout({
         <link
           rel="stylesheet"
           href="styles/Font/FontAwesome/all.min.css"
-        ></link> */}
+        ></link>
       </head>
 
       <body>
