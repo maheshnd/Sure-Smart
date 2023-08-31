6599055d4444d40c263b2044b48e47c91d96705e
mahesh <deshmukh.mahesh.n@gmail.com>
2023-08-31 15:49:07 +0530

    added font awsome icons

--- src/app/components/WhatsAppChat.tsx
+++ src/app/components/WhatsAppChat.tsx
@@ -1,32 +1,4 @@
 "use client";
-import { styled, keyframes } from "styled-components";
-const Container = styled.div`
-  position: fixed !important;
-  left: 10px !important;
-  bottom: 50px !important;
-  z-index: 1000 !important;
-`;
-
-const bounceAnimation = keyframes`
-  0%, 100% {
-    transform: translateY(0);
-    color: #28a745;
-  }
-  50% {
-    transform: translateY(-15px);
-    color: #339933;
-  }
-`;
-
-const WhatsAppIcon = styled.a`
-  animation: ${bounceAnimation} 1s infinite !important;
-  cursor: pointer !important;
-  display: inline-block !important; /* Ensure the link takes up space */
-`;
-
-const Icon = styled.i`
-  font-size: 4rem !important;
-`;
 export const WhatsAppChat = () => {
   const handleWhatsAppClick = () => {
     const phoneNumber = "7350837127";
@@ -37,10 +9,10 @@ export const WhatsAppChat = () => {
     window.open(whatsappURL, "_blank");
   };
   return (
-    <Container>
-      <WhatsAppIcon onClick={handleWhatsAppClick}>
-        <Icon className="fab fa-whatsapp"></Icon>
-      </WhatsAppIcon>
-    </Container>
+    <div className="whatapp-Container">
+      <a className="whatsapp-icon-link" onClick={handleWhatsAppClick}>
+        <i className="fab fa-whatsapp whatsapp-icon"></i>
+      </a>
+    </div>
   );
 };
