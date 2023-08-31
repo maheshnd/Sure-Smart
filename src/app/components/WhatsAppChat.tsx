dd1bdff20b44b40bf6754a0508d2b7d03d872bfc
mahesh <deshmukh.mahesh.n@gmail.com>
2023-08-31 16:06:35 +0530

    updated whats app click logic

--- src/app/components/WhatsAppChat.tsx
+++ src/app/components/WhatsAppChat.tsx
@@ -1,16 +1,17 @@
-"use client";
 export const WhatsAppChat = () => {
-  const handleWhatsAppClick = () => {
-    const phoneNumber = "7350837127";
-    const message = encodeURIComponent(
-      "Hi, I would like to have a call for counseling."
-    );
-    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
-    window.open(whatsappURL, "_blank");
-  };
+  const phoneNumber = "7350837127";
+  const message = encodeURIComponent(
+    "Hi, I would like to have a call for counseling."
+  );
+  const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
   return (
     <div className="whatapp-Container">
-      <a className="whatsapp-icon-link" onClick={handleWhatsAppClick}>
+      <a
+        className="whatsapp-icon-link"
+        href={whatsappURL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-whatsapp whatsapp-icon"></i>
       </a>
     </div>
