966f8fc82a670b8b9fd3f8eda722ed2b271d50fc
mahesh <deshmukh.mahesh.n@gmail.com>
2023-08-31 15:08:43 +0530

    added font awsome icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,10 @@ export default function RootLayout({
           rel="stylesheet"
           href="./styles/open-iconic-bootstrap.min.css"
         ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
       </head>
 
       <body>
